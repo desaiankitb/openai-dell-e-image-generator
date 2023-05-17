88471bd3ebdacacca4806636715ffd4ea43ad33a
Ankit Desai <ankitdesai@Tarans-MacBook-Pro.local>
2023-05-17 11:58:59 +0530

    clean app.py with proper env variables for keys

--- app.py
+++ app.py
@@ -1,38 +1,22 @@
 import os
 
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
+from src.dell_model import DellEModel
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+dell_model = DellEModel(os.getenv("OPENAI_API_KEY"))
 
-
-@app.route("/", methods=("GET", "POST"))
+@app.route('/')
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+    return render_template('index.html')
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+@app.route('/generate', methods=['POST'])
+def generate():
+    keywords = request.form['keywords'].split(',')
+    print(keywords)
+    image = dell_model.generate_image(keywords)
+    image.save('static/generated_image.jpg')
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
